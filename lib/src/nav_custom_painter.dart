c51574e61782e23382a47f760baa32cdcf039784
SHP92 <34475612+SHP92@users.noreply.github.com>
2021-04-18 15:14:02 +0900

    custom paint shadow
    custom paint shadow

--- lib/src/nav_custom_painter.dart
+++ lib/src/nav_custom_painter.dart
@@ -43,6 +43,7 @@ class NavCustomPainter extends CustomPainter {
       ..lineTo(size.width, size.height)
       ..lineTo(0, size.height)
       ..close();
+    canvas.drawShadow(path.shift(Offset(0, -5)), Colors.black26, 2.0, true);
     canvas.drawPath(path, paint);
   }
 
